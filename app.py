442cd6c6ef7796a5739135572ca1cb66a2d39d5c
nidaltrader01 <nidalshwagfeh@gmail.com>
2026-05-19 13:28:21 +0300

    Integrate Finnhub API for stock price retrieval
    Added FINNHUB NVDA PRICE COMMAND

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
-
+FINNHUB_API_KEY = os.environ.get("FINNHUB_API_KEY")
 @app.route("/")
 def home():
     return "Nidal Trader Bot is running"
@@ -15,7 +15,15 @@ def home():
 def webhook():
     data = request.get_json(silent=True) or {}
     message = data.get("message", "TradingView alert received")
+if message == "price":
+    finnhub_url = f"https://finnhub.io/api/v1/quote?symbol=NVDA&token={FINNHUB_API_KEY}"
 
+    response = requests.get(finnhub_url)
+    stock_data = response.json()
+
+    current_price = stock_data.get("c", "N/A")
+
+    message = f"NVDA current price: {current_price}"
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
     payload = {
